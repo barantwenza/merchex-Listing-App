884b346cc22fdcf018271fd5b8a2bb93fd043eaf
barantwenza <kazimabenny@gmail.com>
2023-08-11 14:02:38 +0200

    hello funct Modified, add about, listin functions

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,5 +1,25 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from listings.models import Band
+from listings.models import TitlesBand
 
 def hello(request):
-    return HttpResponse('<h1> Hello Django.</h1>')
+    bands = Band.objects.all()
+    return render(request, 'listings/hello.html', {'bands': bands})
+
+def about(request):
+    return HttpResponse('<h1>About</h1> <p>About us page</p>')
+
+def listin(request):
+    tit = TitlesBand.objects.all()
+    return HttpResponse(f"""<h1> Listings</h1>
+                            <p>Titles</p>
+                            <ul>
+                                <li>{tit[0].title}</li>
+                                <li>{tit[1].title}</li>
+                                <li>{tit[2].title}</li>
+                            </ul>
+                        """)
+
+def contact(request):
+    return HttpResponse('')
